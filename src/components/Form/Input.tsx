f83cdf8d8242d63b40ed3060ba3519d0a7f1dabe
Leonardo <leonardomlouzas@gmail.com>
2022-01-17 22:14:57 -0300

    feat: adds customization into component input

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -8,21 +8,49 @@ import {
   InputGroup,
 } from "@chakra-ui/react";
 
-import { useState, useEffect, useCallback, useRef } from "react";
-
 import { FaExclamation } from "react-icons/fa";
 
-export const Input = () => {
+import { useState, useEffect, useCallback, useRef } from "react";
+import { FieldError } from "react-hook-form";
+import { IconType } from "react-icons";
+
+import { string } from "yup";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  error?: FieldError | null;
+  icon?: IconType;
+}
+export const Input = ({
+  name,
+  error = null,
+  icon: Icon,
+  label,
+  ...rest
+}: InputProps) => {
   return (
     <FormControl>
-      <FormLabel>Label</FormLabel>
-      <InputGroup flexDirection="column">
-        <InputLeftElement mt="2.5">
-          <FaExclamation />
-        </InputLeftElement>
-        <ChakraInput placeholder="Email" />
+      {!!label && <FormLabel>{label}</FormLabel>}
 
-        <FormErrorMessage>Erro</FormErrorMessage>
+      <InputGroup flexDirection="column">
+        {Icon && (
+          <InputLeftElement mt="2.5">
+            <Icon />
+          </InputLeftElement>
+        )}
+        <ChakraInput
+          name={name}
+          b="gray.50"
+          variant="outline"
+          _hover={{ bgColor: "gray.100" }}
+          _placeholder={{ color: "gray.300" }}
+          size="lg"
+          h="60px"
+          {...rest}
+        />
+
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
       </InputGroup>
     </FormControl>
   );
